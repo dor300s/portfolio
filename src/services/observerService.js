0efa952f2f72b9718502afa07f1e0fd594fdbc9a
Dor <dor300s@gmail.com>
2021-04-27 19:47:27 +0300

    scroll fix

--- src/services/observerService.js
+++ src/services/observerService.js
@@ -2,7 +2,6 @@ export const observer = new IntersectionObserver((enteries) => {
     if (enteries[0].isIntersecting) {
         // console.log(enteries[0]);
         enteries[0].target.classList.add('in-view');
-        // console.log(observer);
     } else {
         enteries[0].target.classList.remove('in-view');
     };
