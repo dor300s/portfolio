b042085c39b684c73c66da39a6983cc70642993f
Dor <dor300s@gmail.com>
2021-05-03 23:27:29 +0300

    profile via css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import profilePicture from "../images/Profile.png";
+// import profilePicture from "../images/Profile.png";
 
 export const About = () => {
 
@@ -7,7 +7,7 @@ export const About = () => {
     <div className="about-page-container flex column">
       <h3 className="title">ABOUT ME</h3>
       <div className="main-content flex column justify-center align-center">
-        <img src={profilePicture} alt="#" />
+        <img alt="Profile image"/>
         <p>{`Full Stack Web Developer, passionate about
              solving challenges, Quick learner and hard-working individual,
              Team player, Excellent analytical, Good communication skills.
